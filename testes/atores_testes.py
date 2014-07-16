4c07113f096625843cb2881b5c4f59e9c4805044
renzon <renzon@gmail.com>
2014-07-16 14:48:15 -0300

    Teste de colisão entre atores ativos

--- testes/atores_testes.py
+++ testes/atores_testes.py
@@ -14,10 +14,33 @@ class AtorTestes(TestCase):
         ator = Ator(0.6, 2.1)
         self.assertTupleEqual((1, 2, 'A'), ator.calcular_posicao(3.14))
 
-    def test_status(self):
+    def teste_status(self):
         ator = Ator()
         self.assertEqual(ATIVO, ator.status)
         self.assertEqual('A', ator.calcular_posicao(0)[2])
         ator.status = DESTRUIDO
         self.assertEqual('✝', ator.calcular_posicao(0)[2])
         self.assertEqual(ATIVO, Ator().status)
+
+    def assert_colisao_atores_ativos(self, ator, ator2):
+        self.assertEqual(ator.status, ATIVO)
+        self.assertEqual(ator2.status, ATIVO)
+        self.assertTrue(ator.colidiu(ator2))
+        self.assertEqual(ator2.status, DESTRUIDO)
+        self.assertEqual(ator.status, DESTRUIDO)
+
+
+    def teste_colisao_entre_atores_ativos(self):
+        ator = Ator(2, 2)
+        ator2 = Ator(2, 2)
+        self.assert_colisao_atores_ativos(ator, ator2)
+        self.assert_colisao_atores_ativos(Ator(2, 2), Ator(2, 3))
+        self.assert_colisao_atores_ativos(Ator(2, 2), Ator(3, 3))
+        self.assert_colisao_atores_ativos(Ator(2, 2), Ator(3, 2))
+        self.assert_colisao_atores_ativos(Ator(2, 2), Ator(3, 1))
+        self.assert_colisao_atores_ativos(Ator(2, 2), Ator(2, 1))
+        self.assert_colisao_atores_ativos(Ator(2, 2), Ator(1, 1))
+        self.assert_colisao_atores_ativos(Ator(2, 2), Ator(1, 2))
+        self.assert_colisao_atores_ativos(Ator(2, 2), Ator(1, 3))
+
+
