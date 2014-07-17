34945da6c8af85a44b1e41709f1242bd89e57357
renzon <renzon@gmail.com>
2014-07-16 22:14:19 -0300

    Adição de pássaros Ok

--- fase.py
+++ fase.py
@@ -5,7 +5,15 @@ from __future__ import unicode_literals
 
 class Fase():
     def __init__(self):
+        self._passaros = []
+        self._porcos = []
         self._obstaculos = []
 
     def adicionar_obstaculo(self, *obstaculos):
         self._obstaculos.extend(obstaculos)
+
+    def adicionar_porco(self, *porcos):
+        self._porcos.extend(porcos)
+
+    def adicionar_passaro(self, *passaros):
+        self._passaros.extend(passaros)
--- testes/fase_testes.py
+++ testes/fase_testes.py
@@ -2,7 +2,7 @@
 
 from __future__ import unicode_literals
 from unittest.case import TestCase
-from atores import Obstaculo, Porco
+from atores import Obstaculo, Porco, PassaroVermelho, PassaroAmarelo
 from fase import Fase
 
 
@@ -17,7 +17,7 @@ class FaseTestes(TestCase):
         obstaculo1, obstaculo2 = Obstaculo(), Obstaculo()
         fase.adicionar_obstaculo(obstaculo1, obstaculo2)
         self.assertListEqual([obstaculo, obstaculo1, obstaculo2], fase._obstaculos)
-        
+
     def teste_adicionar_porco(self):
         fase = Fase()
         self.assertListEqual([], fase._porcos)
@@ -28,4 +28,15 @@ class FaseTestes(TestCase):
         porco1, porco2 = Porco(), Porco()
         fase.adicionar_porco(porco1, porco2)
         self.assertListEqual([porco, porco1, porco2], fase._porcos)
+
+    def teste_adicionar_passaro(self):
+        fase = Fase()
+        self.assertListEqual([], fase._passaros)
+        passaro = PassaroVermelho()
+        fase.adicionar_passaro(passaro)
+        self.assertListEqual([passaro], fase._passaros)
+
+        passaro1, passaro2 = PassaroVermelho(), PassaroAmarelo()
+        fase.adicionar_passaro(passaro1, passaro2)
+        self.assertListEqual([passaro, passaro1, passaro2], fase._passaros)
 
