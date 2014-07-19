83f65bc83481a46b467c5158de0ae912e3b327b1
renzon <renzon@gmail.com>
2014-07-19 01:40:58 -0300

    Teste pegando bug com passaros sobrando e porcos todos inativos

--- rodar_fase_exemplo.py
+++ rodar_fase_exemplo.py
@@ -5,26 +5,24 @@ from atores import PassaroAmarelo, PassaroVermelho, Obstaculo, Porco
 from fase import Fase
 from placa_grafica_tkinter import rodar_fase
 
-fase = Fase(intervalo_de_colisao=10)
+if __name__=='__main__':
+    fase = Fase(intervalo_de_colisao=10)
 
 
-# Adicion Pássaros Vermelhos
-for i in range(5):
-    fase.adicionar_passaro(PassaroVermelho(30, 30))
-# Adicion Pássaros Amarelos
-for i in range(30):
-    fase.adicionar_passaro(PassaroAmarelo(30, 30))
+    # Adicionar Pássaros Vermelhos
+    for i in range(5):
+        fase.adicionar_passaro(PassaroVermelho(30, 30))
+    # Adicionar Pássaros Amarelos
+    for i in range(30):
+        fase.adicionar_passaro(PassaroAmarelo(30, 30))
 
 
-#Obstaculos
-for i in range(30, 480, 32):
-    fase.adicionar_obstaculo(Obstaculo(300, i))
+    # Obstaculos
+    for i in range(30, 480, 32):
+        fase.adicionar_obstaculo(Obstaculo(300, i))
 
-# Porcos
-for i in range(30, 300,32):
-    fase.adicionar_porco(Porco(600, i))
+    # Porcos
+    for i in range(30, 300, 32):
+        fase.adicionar_porco(Porco(600, i))
 
-
-
-
-rodar_fase(fase)
+    rodar_fase(fase)
