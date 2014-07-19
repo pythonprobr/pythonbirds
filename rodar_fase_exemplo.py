82471aa6fc072bde4da83ed96b1ac5329ce9be93
renzon <renzon@gmail.com>
2014-07-19 01:23:58 -0300

    Fase de Exemplo

---
+++ rodar_fase_exemplo.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from atores import PassaroAmarelo, PassaroVermelho, Obstaculo, Porco
+from fase import Fase
+from placa_grafica_tkinter import rodar_fase
+
+fase = Fase()
+
+
+# Adicion Pássaros Vermelhos
+for i in range(5):
+    fase.adicionar_passaro(PassaroVermelho(30, 30))
+# Adicion Pássaros Amarelos
+for i in range(30):
+    fase.adicionar_passaro(PassaroAmarelo(30, 30))
+
+
+#Obstaculos
+for i in range(30, 480, 32):
+    fase.adicionar_obstaculo(Obstaculo(300, i))
+
+# Porcos
+for i in range(30, 300,32):
+    fase.adicionar_porco(Porco(600, i))
+
+
+
+
+rodar_fase(fase)
