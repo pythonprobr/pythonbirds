f67b4c455aa0f67990693e0b2394ce2cbe1190e7
renzon <renzon@gmail.com>
2014-09-01 09:56:41 -0300

    Documentação da classe fase

--- fase.py
+++ fase.py
@@ -18,6 +18,11 @@ class Ponto():
 
 class Fase():
     def __init__(self, intervalo_de_colisao=1):
+        """
+        Método que inicializa uma fase.
+
+        :param intervalo_de_colisao:
+        """
         self.intervalo_de_colisao = intervalo_de_colisao
         self._passaros = []
         self._porcos = []
@@ -27,18 +32,54 @@ class Fase():
         lista.extend(atores)
 
     def adicionar_obstaculo(self, *obstaculos):
+        """
+        Adiciona obstáculos em uma fase
+
+        :param obstaculos:
+        """
         self._adicionar_ator(self._obstaculos, *obstaculos)
 
     def adicionar_porco(self, *porcos):
+        """
+        Adiciona porcos em uma fase
+
+        :param porcos:
+        """
         self._adicionar_ator(self._porcos, *porcos)
 
     def adicionar_passaro(self, *passaros):
+        """
+        Adiciona pássaros em uma fase
+
+        :param passaros:
+        """
         self._adicionar_ator(self._passaros, *passaros)
 
     def acabou(self):
+        """
+        Método que retorna verdadeiro se o jogo acabou e falso caso contrário
+
+        O jogo pode acabar por dois motivos:
+
+        1. Não existem mais porcos ativos no jogo
+        2. Não existem mais pássaros ativos no jogo
+
+        :return: booleano
+        """
         return not self._existe_porco_ativo() or not self._existe_passaro_ativo()
 
     def status(self):
+        """
+        Método que indica com mensagem o status do jogo
+
+        Se o jogo está em andamento (ainda tem porco ativo e pássaro ativo), retorna essa mensagem.
+
+        Se o jogo acabou com derrota (ainda existe porco ativo), retorna essa mensagem
+
+        Se o jogo acabou com vitória (não existe porco ativo), retorna essa mensagem
+
+        :return:
+        """
         if not self._existe_porco_ativo():
             return 'Jogo em encerrado. Você ganhou!'
         if self._existe_passaro_ativo():
@@ -46,6 +87,16 @@ class Fase():
         return 'Jogo em encerrado. Você perdeu!'
 
     def lancar(self, angulo, tempo):
+        """
+        Método que executa lógica de lançamento.
+
+        Deve escolher o primeiro pássaro não lançado da lista e chamar seu método lançar
+
+        Se não houver esse tipo de pássaro, não deve fazer nada
+
+        :param angulo: ângulo de lançamento
+        :param tempo: Tempo de lançamento
+        """
         for passaro in self._passaros:
             if not passaro.foi_lancado():
                 passaro.lancar(angulo, tempo)
@@ -53,6 +104,14 @@ class Fase():
 
 
     def calcular_pontos(self, tempo):
+        """
+        Lógica que retorna os pontos a serem exibidos na tela.
+
+        Cada ator deve ser transformado em um Ponto.
+
+        :param tempo: tempo para o qual devem ser calculados os pontos
+        :return: objeto do tipo Ponto
+        """
         pontos = [self._calcular_ponto_de_passaro(p, tempo) for p in self._passaros]
         obstaculos_e_porcos = chain(self._obstaculos, self._porcos)
         pontos.extend([self._transformar_em_ponto(ator) for ator in obstaculos_e_porcos])
