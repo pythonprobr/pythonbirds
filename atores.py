7a4ca5be8919a1cdd9c0e4c12cd06a4501fece66
Otavio Carneiro <otavio@carneiro.blog.br>
2016-01-10 11:40:12 -0200

    Revertendo a diversão

--- atores.py
+++ atores.py
@@ -1,7 +1,126 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+import math
 
 DESTRUIDO = 'Destruido'
 ATIVO = 'Ativo'
 
 
+class Ator():
+    _caracter_ativo = 'A'
+    _caracter_destruido = ' '
+
+    def __init__(self, x=0, y=0):
+        self.y = y
+        self.x = x
+        self._tempo_de_colisao = None
+
+    def caracter(self, tempo):
+        return self._caracter_ativo if self.status(tempo) == ATIVO else self._caracter_destruido
+
+    def resetar(self):
+        self._tempo_de_colisao = None
+
+    def status(self, tempo):
+        if self._tempo_de_colisao is None or self._tempo_de_colisao > tempo:
+            return ATIVO
+        return DESTRUIDO
+
+    def calcular_posicao(self, tempo):
+        return self.arredondar_posicao()
+
+    def arredondar_posicao(self):
+        self.x, self.y = round(self.x), round(self.y)
+        return self.x, self.y
+
+    def colidir(self, outro_ator, tempo, intervalo=1):
+        if self.status(tempo) == DESTRUIDO or outro_ator.status(tempo) == DESTRUIDO:
+            return
+        x1, y1 = self.arredondar_posicao()
+        x2, y2 = outro_ator.arredondar_posicao()
+
+        if x1 - intervalo <= x2 <= x1 + intervalo and y1 - intervalo <= y2 <= y1 + intervalo:
+            self._tempo_de_colisao = tempo
+            outro_ator._tempo_de_colisao = tempo
+
+
+class Obstaculo(Ator):
+    _caracter_ativo = 'O'
+
+
+class Porco(Ator):
+    _caracter_ativo = '@'
+    _caracter_destruido = '+'
+
+
+GRAVIDADE = 10  # m/s^2
+
+
+class Passaro(Ator):
+    velocidade_escalar = None
+
+    def __init__(self, x=0, y=0):
+        super().__init__(x, y)
+        self._x_inicial = x
+        self._y_inicial = y
+        self._tempo_de_lancamento = None
+        self._angulo_de_lancamento = None  # radianos
+
+    def resetar(self):
+        super().resetar()
+        self._tempo_de_lancamento = None
+        self._angulo_de_lancamento = None
+
+
+    def foi_lancado(self):
+        return self._tempo_de_lancamento is not None
+
+    def colidir_com_chao(self, tempo):
+        if self.y <= 0:
+            self._tempo_de_colisao = tempo
+
+    def _calcular_posicao_horizontal(self, delta_t):
+        self.x = self._x_inicial + self.velocidade_escalar * delta_t * math.cos(self._angulo_de_lancamento)
+
+    def _calcular_posicao_vertical(self, delta_t):
+        self.y = (self._y_inicial +
+                  self.velocidade_escalar * delta_t * math.sin(self._angulo_de_lancamento) -
+                  (GRAVIDADE / 2) * delta_t ** 2)
+
+    def _calcular_posicao(self, tempo):
+        delta_t = tempo - self._tempo_de_lancamento
+        self._calcular_posicao_vertical(delta_t)
+        self._calcular_posicao_horizontal(delta_t)
+
+    def calcular_posicao(self, tempo):
+        if self._aguardando_lancamento(tempo):
+            self.x = self._x_inicial
+            self.y = self._y_inicial
+        elif self._ja_colidiu(tempo):
+            self._calcular_posicao(self._tempo_de_colisao)
+        else:
+            self._calcular_posicao(tempo)
+        return self.arredondar_posicao()
+
+    def lancar(self, angulo, tempo):
+        self._tempo_de_lancamento = tempo
+        self._angulo_de_lancamento = math.radians(angulo)
+
+    def _aguardando_lancamento(self, tempo):
+        return not self.foi_lancado() or tempo < self._tempo_de_lancamento
+
+    def _ja_colidiu(self, tempo):
+        return self.foi_lancado() and self.status(tempo) == DESTRUIDO
+
+
+class PassaroAmarelo(Passaro):
+    velocidade_escalar = 30  # m/s
+    _caracter_ativo = 'A'
+    _caracter_destruido = 'a'
+
+
+class PassaroVermelho(Passaro):
+    velocidade_escalar = 20  # m/s
+    _caracter_ativo = 'V'
+    _caracter_destruido = 'v'
--- fase.py
+++ fase.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from itertools import chain
 from atores import ATIVO
 
 
@@ -14,3 +15,73 @@ class Ponto():
     def __repr__(self, *args, **kwargs):
         return "Ponto(%s,%s,'%s')" % (self.x, self.y, self.caracter)
 
+
+class Fase():
+    def __init__(self, intervalo_de_colisao=1):
+        self.intervalo_de_colisao = intervalo_de_colisao
+        self._passaros = []
+        self._porcos = []
+        self._obstaculos = []
+
+    def _adicionar_ator(self, lista, *atores):
+        lista.extend(atores)
+
+    def adicionar_obstaculo(self, *obstaculos):
+        self._adicionar_ator(self._obstaculos, *obstaculos)
+
+    def adicionar_porco(self, *porcos):
+        self._adicionar_ator(self._porcos, *porcos)
+
+    def adicionar_passaro(self, *passaros):
+        self._adicionar_ator(self._passaros, *passaros)
+
+    def acabou(self, tempo):
+        return not self._existe_porco_ativo(tempo) or not self._existe_passaro_ativo(tempo)
+
+    def status(self, tempo):
+        if not self._existe_porco_ativo(tempo):
+            return 'Jogo em encerrado. Você ganhou!'
+        if self._existe_passaro_ativo(tempo):
+            return 'Jogo em andamento.'
+        return 'Jogo em encerrado. Você perdeu!'
+
+    def lancar(self, angulo, tempo):
+        for passaro in self._passaros:
+            if not passaro.foi_lancado():
+                passaro.lancar(angulo, tempo)
+                return
+
+    def resetar(self):
+        for ator in chain(self._passaros, self._obstaculos, self._porcos):
+            ator.resetar()
+
+    def calcular_pontos(self, tempo):
+        pontos = [self._calcular_ponto_de_passaro(p, tempo) for p in self._passaros]
+        obstaculos_e_porcos = chain(self._obstaculos, self._porcos)
+        pontos.extend([self._transformar_em_ponto(ator, tempo) for ator in obstaculos_e_porcos])
+        return pontos
+
+    def _transformar_em_ponto(self, ator, tempo):
+        return Ponto(ator.x, ator.y, ator.caracter(tempo))
+
+    def _calcular_ponto_de_passaro(self, passaro, tempo, ):
+        passaro.calcular_posicao(tempo)
+        for ator in chain(self._obstaculos, self._porcos):
+            if ATIVO == passaro.status(tempo):
+                passaro.colidir(ator, tempo, self.intervalo_de_colisao)
+                passaro.colidir_com_chao(tempo)
+            else:
+                break
+        return self._transformar_em_ponto(passaro, tempo)
+
+    def _existe_porco_ativo(self, tempo):
+        return self._verificar_se_existe_ator_ativo(self._porcos, tempo)
+
+    def _verificar_se_existe_ator_ativo(self, atores, tempo):
+        for a in atores:
+            if a.status(tempo) == ATIVO:
+                return True
+        return False
+
+    def _existe_passaro_ativo(self, tempo):
+        return self._verificar_se_existe_ator_ativo(self._passaros, tempo)
