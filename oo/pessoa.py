2cac9352e5b2899e6f173fd77fb2076bf822dc56
kennedimalheiros <kennedimalheiros@gmail.com>
2020-01-24 15:21:01 -0300

    Creating instance attributes name and age

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa():
+    def __init__(self, nome = None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('João')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Kennedi'
+    print(p.nome)
