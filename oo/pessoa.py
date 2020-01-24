846adaf990b520c49d95a30c0f7e05d82ec904ba
kennedimalheiros <kennedimalheiros@gmail.com>
2020-01-24 15:40:44 -0300

    creating child complex attribute

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,20 @@
 class Pessoa():
-    def __init__(self, nome = None, idade=35):
+    def __init__(self, *filhos, nome = None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('João')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Kennedi'
-    print(p.nome)
+    joaozinho = Pessoa(nome='Joãozinho')
+    joao = Pessoa(joaozinho, nome='João')
+    print(Pessoa.cumprimentar(joao))
+    print(id(joao))
+    print(joao.cumprimentar())
+    print(joao.nome)
+    print(joao.idade)
+    for filho in joao.filhos:
+        print(f'filho {filho.nome}')
+   
