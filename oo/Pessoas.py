1b0a0faf1f47f0d4d4fd9fd240db5dc67c5ae322
Alex Oliveira <alex.borgesoliveira@gmail.com>
2020-05-06 17:48:55 -0300

    Criado atributo de instancias nome e idade

--- oo/Pessoas.py
+++ oo/Pessoas.py
@@ -13,4 +13,21 @@ As classes devem sempre começar com a primeira letra em Maiusculo ,
 """
 
 class Pessoas:
-    pass
+    def __init__(self, nome=None , idade = 45): #Nome nesssa opção é um parametro da função
+        self.idade = idade
+        self.nome = nome # Definindo um atriburo com vaor nulo, nesse caso o self.nome é um atributo
+                            # da função.
+        
+    def cumprimentar(self):
+        return f'Olá '
+    
+if __name__ == '__main__':
+    p = Pessoas('Alex')
+    # Opçào 1 para imprimir um objeto
+    print('Uma opção para imprimir o objeto')
+    print(Pessoas.cumprimentar(p))
+    # Opção 2 para imprimir um objeto , sendo esse o mais pratico e usual.
+    print('Outra opção para imprimir o objeto')
+    print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
