acb743789bd65fb51e14accacf896609348f13c9
Alex Oliveira <alex.borgesoliveira@gmail.com>
2020-05-06 11:31:50 -0300

    Criado o pacote OO com arquivo Pessoas.py com modelo de como deve ser criado uma classe.

---
+++ oo/Pessoas.py
@@ -0,0 +1,16 @@
+"""
+Exemplo de como deve ser criado uma class em Python
+As classes devem sempre começar com a primeira letra em Maiusculo ,
+  caso seja uma palavra composta a primeira letra de cada palavra
+  deve iniciar em Maiusculo.
+  Ex.: De classes
+        class FrutasCitricas:
+            pass - Essa opção é usada para deixar uma classe funcional
+                   e temporareamene sem dados.
+        
+        class Fturas:
+            pass
+"""
+
+class Pessoas:
+    pass
