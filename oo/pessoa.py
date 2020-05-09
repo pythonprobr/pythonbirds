d6cfb85167bfd5552fc234099033305a4214634c
Tui <larmc20@yahoo.com.br>
2020-05-09 17:13:40 -0300

    adicionado objetos complexos aos atributos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 
 class Pessoa:
+    def __init__(self, *filhos, nome=None,idade=None):
+        self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == "__main__":
-    p = Pessoa()
-    print(p.cumprimentar())
+    renzo = Pessoa(nome="Renzo", idade=35)
+    fabio = Pessoa(renzo,nome="Fábio", idade=35)
+    
+    print(fabio.nome, fabio.idade)
+
+    for filhos in fabio.filhos:
+        print(filhos.nome)
