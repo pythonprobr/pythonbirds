8222def55bca11ea313376b8c87c8b16107e5cab
Alex Oliveira <alex.borgesoliveira@gmail.com>
2020-05-09 22:57:15 -0300

    Implementada Direção

--- oo/carro.py
+++ oo/carro.py
@@ -1,3 +1,4 @@
+
 """
 Você deve criar uma classe carro que vai possuir
 dois atributos compostos por outras duas classes:
@@ -93,3 +94,33 @@ Exemplo:
     >>> carro.calcular_direcao()
     'Oeste'
 """
+
+class Motor:
+    def __init__(self):
+        self.velocidade = 0
+    
+    def acelerar(self):
+        self.velocidade += 1
+    
+    def frear(self):
+        self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
+class Direcao:
+    rotacao_a_direita_dct = {NORTE : LESTE , LESTE : SUL , SUL : OESTE , OESTE : NORTE}
+    rotacao_a_esquera_dct = {NORTE : OESTE, LESTE : NORTE, SUL : LESTE, OESTE : SUL}
+    
+    def __init__(self):
+        self.valor = NORTE
+        
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+        
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquera_dct[self.valor]
+        
+    
