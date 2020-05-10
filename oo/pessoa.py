7adc5bc32da508965a840bad520decaf4be84322
Hernany Santos <hernanyed@gmail.com>
2020-05-10 17:25:46 -0300

    Criado atributo da Classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,19 @@
 class Pessoa:
+    # Executado quanto é construída a classe
+    def __init__(self, nome=None, idade=39):
+        # Atributos da Classe
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Hernany')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Santos'
+    print(p.nome)
+    print(p.idade)
