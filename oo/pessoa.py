35220365e4999c85456ebfa078d37091962595db
Hernany Santos <hernanyed@gmail.com>
2020-05-10 17:34:11 -0300

    Criado atributo complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,23 @@
 class Pessoa:
     # Executado quanto é construída a classe
-    def __init__(self, nome=None, idade=39):
+    def __init__(self, *filhos, nome=None, idade=39):
         # Atributos da Classe
         self.idade = idade
         self.nome = nome
+        # Atributos complexos : Listas, Dicionários, Tuplas
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Hernany')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Santos'
-    print(p.nome)
-    print(p.idade)
+    pedro = Pessoa(nome='Pedro Soares Santos')
+    # Passo o atributo de filhos e o atributo nomeado
+    hernany = Pessoa(pedro, nome='Hernany Santos')
+    print(Pessoa.cumprimentar(hernany))
+    print(id(hernany))
+    print(hernany.cumprimentar())
+    print(hernany.nome)
+    print(hernany.idade)
+    for filho in hernany.filhos:
+        print(filho.nome)
