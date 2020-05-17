9e597bda02a70d776bf9d9a1ad033f31c919143a
Hernany Santos <hernanyed@gmail.com>
2020-05-17 19:41:28 -0300

    Resolução execicio carro, direção, motor

--- oo/carro.py
+++ oo/carro.py
@@ -1,7 +1,48 @@
+class Motor:
+    # Responsabilidade de controlar a velocidade
+    def __init__(self):
+        self.velocidade = 0
+
+    def acelerar(self):
+       self.velocidade += 1
+       return self.velocidade
+
+    def frear(self):
+       self.velocidade -= 2
+       if self.velocidade < 0:
+           self.velocidade = 0
+       return self.velocidade
+
+class Direcao:
+    # Responsabilidade de controlar a direção
+    def __init__(self):
+        self.valor = 'Norte'
+        self.posicao = 0
+        self.direcoes = ['Norte', 'Leste', 'Sul', 'Oeste']
 
 
-from oo.motor import Motor
-from oo.direcao import Direcao
+    def mostra_direcao(self):
+        return self.direcoes[self.posicao]
+    
+    def direita(self):
+        self.posicao += 1
+        if self.posicao <= 3:
+            self.valor = self.direcoes[self.posicao]
+        else:
+            self.posicao = 0
+            self.valor = self.direcoes[self.posicao]
+
+        return self.valor
+
+    def esquerda(self):
+        self.posicao -= 1
+        if self.posicao >= 0:
+            self.valor = self.direcoes[self.posicao]
+        else:
+            self.posicao = len(self.direcoes) - 1
+            self.valor = self.direcoes[self.posicao]
+
+        return self.valor
 
 class Carro:
 
@@ -12,8 +53,11 @@ class Carro:
     def calcular_velocidade(self):
         return self.motor.velocidade
     
-    def calcular_acelerar(self):
+    def acelerar(self):
         return self.motor.acelerar()
+    
+    def frear(self):
+        return self.motor.frear()
 
     def calcular_direcao(self):
         return self.direcao.valor
@@ -26,10 +70,23 @@ class Carro:
 
 
 if __name__ == '__main__':
-    carro = Carro(Motor(), Direcao())
+    direcao = Direcao()
+    motor = Motor()
+    carro = Carro(direcao, motor)
     print('Testando o Motor')
-    print(carro.calcular_velocidade)
+    print(carro.calcular_velocidade())
+    print(carro.acelerar())
+    print(carro.acelerar())
+    print(carro.acelerar())
+    print(carro.frear())
     print('Testando a Direção')
     print(carro.calcular_direcao())
     print(carro.girar_a_direita())
+    print(carro.girar_a_direita())
+    print(carro.girar_a_esquerda())
+    print(carro.girar_a_direita())
+    print(carro.girar_a_direita())
+    print(carro.girar_a_direita())
+    print(carro.girar_a_esquerda())
+    print(carro.girar_a_esquerda())
     print(carro.girar_a_esquerda())
