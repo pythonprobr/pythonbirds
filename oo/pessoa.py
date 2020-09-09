490f25ee088c1cd03d578923e50275b0b322503d
Henrique <luizhenrique.gomess@hotmail.com>
2020-09-09 17:00:47 -0300

    Criado método para compreender classes

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,8 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return 'Ola, tudo certo contigo bro?'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(p.cumprimentar())
+    print(type(p))
