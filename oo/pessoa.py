0abdf12b223a72310663dba8a3437840d20bc412
Henrique <luizhenrique.gomess@hotmail.com>
2020-09-09 17:28:37 -0300

    Criado a subclasse de filhos dentro da classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,23 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = None, altura = None):
+    def __init__(self, *filhos, nome = None, idade = None, altura = None):
         self.altura = altura
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
+    def __str__(self):
+        return f'{self.filhos}'
 
     def cumprimentar(self):
         return 'Ola, tudo certo contigo bro?'
 
 if __name__ == '__main__':
-    p = Pessoa('Junio')
-    print(p.nome)
-    print(p.cumprimentar())
-    print(type(p))
-    p.nome = 'Henrique'
-    print(p.nome)
+    seiya = Pessoa(nome = 'Seiya')
+    amanda = Pessoa(nome = 'Amanda')
+    tohka = Pessoa(nome = 'Tohka')
+    henrique = Pessoa(seiya, amanda, tohka, nome = 'Henrique')
+    print(henrique.nome)
+    print(f'----Os filhos de {henrique.nome} são:')
+    for filhos in henrique.filhos:
+        print(f'--------{filhos.nome}--------')
+
