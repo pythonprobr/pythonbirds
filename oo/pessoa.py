45fa9f4d21b962e37dff90f945bf4765fb6d58b9
Henrique <luizhenrique.gomess@hotmail.com>
2020-09-09 17:15:42 -0300

    Denvolvido forma de atribuir caracteristicas ao objeto através do __init__, nome;idade; altura

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,16 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = None, altura = None):
+        self.altura = altura
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return 'Ola, tudo certo contigo bro?'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Junio')
+    print(p.nome)
     print(p.cumprimentar())
     print(type(p))
+    p.nome = 'Henrique'
+    print(p.nome)
