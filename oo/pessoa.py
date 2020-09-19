ed3b6b656e54cace172235d3ab061a2836ba6bcf
Henrique <luizhenrique.gomess@hotmail.com>
2020-09-18 21:48:20 -0300

    Aprendendo Herança de classes

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,7 @@
 class Pessoa:
     olhos = 2
 
-    def __init__(self, *filhos, nome = None, idade = None, altura = None):
+    def __init__(self, *filhos, nome=None, idade=None, altura=None):
         self.altura = altura
         self.idade = idade
         self.nome = nome
@@ -13,20 +13,28 @@ class Pessoa:
     def cumprimentar(self):
         return 'Ola, tudo certo contigo bro?'
 
-    @staticmethod # Método estatico que independe o método da classe, não necessitando depender de objeto algum para ser rodado
+    @staticmethod  # Método estatico que independe o método da classe, não necessitando depender de objeto algum para ser rodado
     def método_estatico():
         return 42
 
-
     @classmethod
-    def nome_e_atributos_de_classe(cls): #Outro método de conseguir descobrir e adquirir informações da classe
+    def nome_e_atributos_de_classe(cls):  # Outro método de conseguir descobrir e adquirir informações da classe
         return f'{cls} - olhos {cls.olhos}'
 
+
+class Homem(Pessoa):
+    pass
+
+
+class Mulher(Pessoa):
+    pass
+
+
 if __name__ == '__main__':
-    seiya = Pessoa(nome = 'Seiya')
-    amanda = Pessoa(nome = 'Amanda')
-    tohka = Pessoa(nome = 'Tohka')
-    henrique = Pessoa(seiya, amanda, tohka, nome = 'Henrique')
+    seiya = Homem(nome='Seiya')
+    amanda = Mulher(nome='Amanda')
+    tohka = Mulher(nome='Tohka')
+    henrique = Homem(seiya, amanda, tohka, nome='Henrique')
     print(henrique.nome)
     print(f'----Os filhos de {henrique.nome} são:')
     for filhos in henrique.filhos:
@@ -34,12 +42,21 @@ if __name__ == '__main__':
     print(tohka.olhos)
     tohka.olhos = 4
     print(tohka.olhos)
-    print(tohka.__dict__) #Instância __dict__ para descobrir os parâmetros e caracteristicas das classes
-    print(henrique.__dict__)#Instância __dict__ para descobrir os parâmetros e caracteristicas das classes
+    print(tohka.__dict__)  # Instância __dict__ para descobrir os parâmetros e caracteristicas das classes
+    print(henrique.__dict__)  # Instância __dict__ para descobrir os parâmetros e caracteristicas das classes
     del tohka.olhos
-    print(tohka.__dict__)#Instância __dict__ para descobrir os parâmetros e caracteristicas das classes
+    print(tohka.__dict__)  # Instância __dict__ para descobrir os parâmetros e caracteristicas das classes
     tohka.sobrenome = 'Kurosaki'
     print(f'--------{tohka.nome} {tohka.sobrenome}--------')
     print()
     print(Pessoa.método_estatico(), henrique.método_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), henrique.nome_e_atributos_de_classe())
+    print()
+    print(isinstance(henrique, Pessoa))
+    print(isinstance(henrique,Homem))
+    print(isinstance(seiya, Pessoa))
+    print(isinstance(seiya, Homem))
+    print(isinstance(amanda, Pessoa))
+    print(isinstance(amanda, Mulher))
+    print(isinstance(tohka, Pessoa))
+    print(isinstance(tohka, Mulher))
---
+++ oo/test_pessoa.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_pessoa(self):
+        self.fail()
+
+    def test_homem(self):
+        self.fail()
+
+    def test_mulher(self):
+        self.fail()
