5b1e65f18e13b244db78b85b4ccc30b1d3e65823
Henrique <luizhenrique.gomess@hotmail.com>
2020-09-18 21:28:26 -0300

    Nesta etapa foi adicionado os objetos do jogo na tela, com suas respectivas classes, mas ainda sem suas imagens verdadeiras

--- fase.py
+++ fase.py
@@ -43,7 +43,7 @@ class Fase():
 
         :param obstaculos:
         """
-        pass
+        self._obstaculos.extend(obstaculos)
 
     def adicionar_porco(self, *porcos):
         """
@@ -51,7 +51,7 @@ class Fase():
 
         :param porcos:
         """
-        pass
+        self._porcos.extend(porcos)
 
     def adicionar_passaro(self, *passaros):
         """
@@ -59,7 +59,7 @@ class Fase():
 
         :param passaros:
         """
-        pass
+        self._passaros.extend(passaros)
 
     def status(self):
         """
