600a7db43ca7dbfdf918dd3c239de273a9e1c49c
Fernando <luiz.fernando_1990@hotmail.com>
2020-11-24 23:57:50 -0300

    Criado aributos de instância nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,19 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':       #utilizado para realizar o teste
-    p = Pessoa()
+    p = Pessoa('luciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Fernando'
+    print(p.nome)
+    print(p.idade)
 
