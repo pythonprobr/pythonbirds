7a6c0008d76cce86a3287b26ad25deadde31c926
Fernando <luiz.fernando_1990@hotmail.com>
2020-11-27 11:58:42 -0300

    Implementada a direção

--- oo/carro.py
+++ oo/carro.py
@@ -12,7 +12,7 @@ Método frear que deverá decrementar a velocidade em duas unidades
 A Direção terá a responsabilidade de controlar a direção. Ela oferece
 os seguintes atributos:
 
-Valor de diração com valores possíveis: Norte, Sul, Leste, Oeste.
+Valor de direção com valores possíveis: Norte, Sul, Leste, Oeste.
 Método girar_a_direita
 Método girar_a_esquerda
 
@@ -92,6 +92,31 @@ Exemplo:
     >>> carro.calcular_direcao()
     'Oeste'
 """
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
+
 NORTE='Norte'
 SUL= 'Sul'
 LESTE = 'Leste'
