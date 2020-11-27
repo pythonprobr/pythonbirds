6e5c26ab0cbdc2193610bd0418191b1949243893
Fernando <luiz.fernando_1990@hotmail.com>
2020-11-27 11:23:52 -0300

    Implementada a direção

--- oo/carro.py
+++ oo/carro.py
@@ -1,4 +1,3 @@
-
 """Você deve criar uma classe carro que vai possuir
 dois atributos compostos por outras duas classes:
 
@@ -97,6 +96,20 @@ NORTE='Norte'
 SUL= 'Sul'
 LESTE = 'Leste'
 OESTE = 'Oeste'
+
+
+class Direcao:
+    rotacao_a_direita_dct = {NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE}
+    rotacao_a_esquerda_dct = {NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL}
+
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
 
 class Motor:
     def __init__(self):
