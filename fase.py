e27a6329c007cb531ebbd5a9c1b01b806bbc341c
Fernando <luiz.fernando_1990@hotmail.com>
2020-12-01 11:10:39 -0300

    Implementada oda a lógica de fase

--- fase.py
+++ fase.py
@@ -108,6 +108,11 @@ class Fase():
         :param tempo: tempo para o qual devem ser calculados os pontos
         :return: objeto do tipo Ponto
         """
+        for passaro in self._passaros:
+            passaro.calcular_posicao(tempo)
+            for alvo in self._obstaculos + self._porcos:
+                passaro.colidir(alvo, self.intervalo_de_colisao)
+            passaro.colidir_com_chao()
         pontos=[self._transformar_em_ponto(a) for a in self._passaros+self._obstaculos+self._porcos]
 
         return pontos
