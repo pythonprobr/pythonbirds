9bb629c54c0033db00bffffad35b37bf639ca7a6
Jeferson Soaris <g7harmonic@outlook.com>
2021-02-26 02:36:39 -0300

    Criado a classe pessoa.

---
+++ oo/pessoa.py
@@ -0,0 +1,27 @@
+'''
+Classe
+*É um molde de instruções contendo um motivo central com sub-propriedades e métodos para chamadas de aplicação;
+*É a base da orientação a objetos;
+*Define a forma como objetos python se comportam;
+*Cada propriedade e método interno podem serem chamados individualmente;
+*É definido com iniciais maiúsculas e CamelCase;
+*Precisa ser instânciada para aplicação;
+*Ela deve ser dinâmica.
+
+Mais sobre classe:
+https://www.youtube.com/embed/cV_Soy7jGns
+https://www.youtube.com/embed/gtgI7PC_TJk
+https://www.youtube.com/watch?v=j6B8shHXzks
+https://pt.wikipedia.org/wiki/Classe_(programa%C3%A7%C3%A3o)
+'''
+
+'1-Criando uma classe:'
+#Usar Cammel Case em caso de duas palavras.
+'''
+Chamando no python console e conferindo a tipagem:
+from oo.pessoa import Pessoa
+classe = Pessoa()
+type(classe)
+'''
+class Pessoa:
+    pass
