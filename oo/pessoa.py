1ae2782dbc01b7d6cdd1c24595e0b0e10e15458f
Jeferson Soaris <g7harmonic@outlook.com>
2021-03-16 21:53:31 -0300

    # Adicionado método cumprimentar()

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,13 @@
 '''
-Classe
+Aqui será abordado: Classe, Métodos e Atributos.
+
+Classe:
 *É um molde de instruções contendo um motivo central com sub-propriedades e métodos para chamadas de aplicação;
 *É a base da orientação a objetos;
 *Define a forma como objetos python se comportam;
 *Cada propriedade e método interno podem serem chamados individualmente;
 *É definido com iniciais maiúsculas e CamelCase;
-*Precisa ser instânciada para aplicação;
+*Precisa ser instanciada para aplicação;
 *Ela deve ser dinâmica.
 
 Mais sobre classe:
@@ -13,15 +15,29 @@ https://www.youtube.com/embed/cV_Soy7jGns
 https://www.youtube.com/embed/gtgI7PC_TJk
 https://www.youtube.com/watch?v=j6B8shHXzks
 https://pt.wikipedia.org/wiki/Classe_(programa%C3%A7%C3%A3o)
+
+Métodos:
+*Métodos são funções construidas e pertencentes a uma classe.
+*Cada função ou método contém suas próprias funcionalidade.
+*Os métodos devem conter a palavra chave "Self" como primeiro parâmetro, sendo isolado ou não.
+
+Artigo sobre o uso do Self: http://archive.is/cX2mq
 '''
 
-'1-Criando uma classe:'
-#Usar Cammel Case em caso de duas palavras.
-'''
-Chamando no python console e conferindo a tipagem:
-from oo.pessoa import Pessoa
-classe = Pessoa()
-type(classe)
-'''
+
+'Criando uma classe e definindo métodos e atributos:'
+# Usar Camel Case em caso de duas palavras.
+
+
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p)) # Modo errado.
+    print(id(p))
+    print(p.cumprimentar()) # Modo correto.
+
+
