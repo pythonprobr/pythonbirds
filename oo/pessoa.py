9007e1380076e54a4f59aede7d0039480c7fa681
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-03-30 16:15:41 -0300

    add atributo de instancia  *filhos na classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f"Olá {id(self)}"
 
+    def __str__(self):
+        return f"Sr(a) {self.nome}"
+
 
 if __name__ == '__main__':
-    p = Pessoa(nome='marcos', idade=45)
+    p = Pessoa(nome='Marcos', idade=45)
+    pessoa_filho = [Pessoa(nome='Layla', idade=21), Pessoa(nome='Isabel', idade=19)]
     print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome, p.idade)
+    for filho in pessoa_filho:
+        print(f" filho de {p}: {filho.nome}, {filho.idade}")
+
+    print(p, p.idade)
