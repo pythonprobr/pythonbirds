0a2b265a502144ef2e80119b5f5b42e848c23659
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-03-30 16:30:04 -0300

    criado e deletado atributo dinamico

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,8 @@
 class Pessoa:
-    def __init__(self, *filhos, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None, sobrenome=None):
         self.nome = nome
         self.idade = idade
+        self.sobrenome = sobrenome
         self.filhos = list(filhos)
 
     def cumprimentar(self):
@@ -12,10 +13,16 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p = Pessoa(nome='Marcos', idade=45)
-    pessoa_filho = [Pessoa(nome='Layla', idade=21), Pessoa(nome='Isabel', idade=19)]
+    p = Pessoa(nome='Marcos', idade=45, sobrenome='Viana')
+    pessoa_filho = [Pessoa(nome='Layla', idade=21, sobrenome=p.sobrenome), Pessoa(nome='Isabel', idade=19, sobrenome=p.sobrenome)]
     print(Pessoa.cumprimentar(p))
+    p.filhos = pessoa_filho
+    p.mid_nome = 'Paulo'
+    print(p.__dict__)
+
+    del p.mid_nome
+    print(p.__dict__)
     for filho in pessoa_filho:
-        print(f" filho de {p}: {filho.nome}, {filho.idade}")
+        print(f" filho de {p}: {filho.nome} {filho.sobrenome}, {filho.idade}")
 
     print(p, p.idade)
