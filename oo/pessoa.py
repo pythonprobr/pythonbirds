6d8f72a34d2bfff431748870cf0ab5c94a0ff581
paulohenriuqe <paulohenrique2@protonmail.com>
2021-08-03 12:42:27 -0300

    criados atributos de instâcia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=26):
+        self.nome = nome
+        self.idade = idade
     def cumprimentar(self):
         return f'ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('henrique')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
-
+    print(p.nome)
+    p.nome = 'paulo'
+    print(p.nome)
+    print(p.idade)
