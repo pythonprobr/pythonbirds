61e33a436c565ffaf065b5082b296d925b4b7141
paulohenriuqe <paulohenrique2@protonmail.com>
2021-08-02 21:11:36 -0300

    criado método cumprimentar

---
+++ oo/pessoa.py
@@ -0,0 +1,11 @@
+class Pessoa:
+    def cumprimentar(self):
+        return f'ola {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+
+
