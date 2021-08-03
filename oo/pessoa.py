6b6c1bebe69005100fcb098bf0cf70ba4fd4e9d6
paulohenriuqe <paulohenrique2@protonmail.com>
2021-08-03 14:36:14 -0300

    criado atributos de classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,6 +6,14 @@ class Pessoa:
         self.filhos = list(filhos)
     def cumprimentar(self):
         return f'ola {id(self)}'
+    @staticmethod
+    def metodo_estatico():
+        return 42
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+
 
 if __name__ == '__main__':
     paulo = Pessoa(nome='Paulo')
@@ -29,3 +37,4 @@ if __name__ == '__main__':
     print(henrique.olhos)
     print(paulo.olhos)
     print(id(Pessoa.olhos),id(henrique.olhos),id(paulo.olhos))
+    print(Pessoa.nome_e_atributos_de_classe(), henrique.nome_e_atributos_de_classe())
