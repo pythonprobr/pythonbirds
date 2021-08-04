2abc9ae46c40e86b8cc9ac489092f7c63cb492a7
paulohenriuqe <paulohenrique2@protonmail.com>
2021-08-04 13:44:29 -0300

    finalizado a implementação do exercicio de composição

--- oo/carro.py
+++ oo/carro.py
@@ -94,6 +94,24 @@ Exemplo:
     'Oeste'
 
 """
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+    def acelerar(self):
+        self.motor.acelerar()
+    def frear(self):
+        self.motor.frear()
+    def calcular_direcao(self):
+        return self.direcao.valor
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
+
 NORTE = 'Norte'
 SUL = 'Sul'
 LESTE = 'Leste'
