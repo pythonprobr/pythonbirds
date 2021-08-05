da1779019c2f93542665cb6d66d470d67c9136d5
paulohenriuqe <paulohenrique2@protonmail.com>
2021-08-05 15:03:21 -0300

    aula sobre o unisttest

---
+++ oo/teste_carro.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from oo.carro import Motor
+
+class CarroTestCase(TestCase):
+    def teste_velocidade_inicial(self):
+        motor = Motor()
+        self.assertEqual(0, motor.velocidade)
+
+    def teste_acelerar(self):
+        motor = Motor()
+        motor.acelerar()
+        self.assertEqual(1, motor.velocidade)
