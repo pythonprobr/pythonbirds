5a8f792faabacd4c22d0b7c5877ebc76af7d64f0
alefenba <alefe.182@outlook.com>
2021-08-24 19:26:35 -0300

    Criado atributo de classe Olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2 
+
     def __init__(self, *filhos, nome = None, idade = 24):
         self.idade = idade
         self.nome = nome
@@ -21,6 +23,17 @@ if __name__ == '__main__':
     print(alefe.filhos)
     alefe.sobrenome = 'Gomes'
     del alefe.filhos
+    alefe.olhos = 1
+    del alefe.olhos
     print(alefe.__dict__)
     print(joao.__dict__)
-    
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(joao.olhos)
+    print(alefe.olhos)
+    print(id(Pessoa.olhos), id(joao.olhos), id(alefe.olhos))
+ 
+
+
+ # Se o valor do atributo for o mesmo de todos objetos ele deve ser um atributo de classe
+ # Se for diferente, ele deve ser criado como atributo de instancia
