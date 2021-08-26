a70254c94c30502d3b87068eb335174cd263a385
alefenba <alefe.182@outlook.com>
2021-08-25 21:23:07 -0300

    Correção do espaçamento em algumas linhas

--- oo/carro.py
+++ oo/carro.py
@@ -22,13 +22,13 @@ from typing_extensions import NotRequired
 
 
 class Motor:
-	def __init__(self):
-		self.velocidade = 0
-  	def  acelerar(self):
-    	self.velocidade += 1
-	def frear(self):
-    	self.velocidade -=2
-    	self.velocidade = max(0,self.velocidade)
+    def __init__(self):
+        self.velocidade = 0
+    def  acelerar(self):
+        self.velocidade += 1
+    def frear(self):
+        self.velocidade -=2
+        self.velocidade = max(0,self.velocidade)
 
 
 #Exemplo:
@@ -58,16 +58,16 @@ OESTE = 'Oeste'
 
 
 class Direcao:
-	rotacao_a_direita_dct= {NORTE: LESTE, LESTE: SUL, SUL: OESTE,OESTE: NORTE}
-	rotacao_a_esquerda_dct= {NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE}
-	def __init__(self):
-        	self.valor = NORTE
-  	
-	def girar_a_direita(self):
-    	self.valor = self.rotacao_a_direita_dct[self.valor]
+    rotacao_a_direita_dct= {NORTE: LESTE, LESTE: SUL, SUL: OESTE,OESTE: NORTE}
+    rotacao_a_esquerda_dct= {NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE}
+    def __init__(self):
+        self.valor = NORTE
+      
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
 
-	def girar_a_esquerda(self):
-      	self.valor = self.rotacao_a_esquerda_dct[self.valor]
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
 
 
 
