6021efe7b0b97016ad2bb9642a1d0062d95a7b2b
Josevaldo10 <mycarima@gmail.com>
2021-12-14 12:07:16 -0300

    Implementação final pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,13 @@
 class Pessoa:
     olhos = 2
+
     def __init__(self,*filhos, nome=None, idade=40):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
 
     def cumprimetar(self):
-        return f'Olá {id(self)}'
+        return f'Olá meu nome é {self.nome}'
 
     @staticmethod
     def metodo_statico():
@@ -14,10 +15,18 @@ class Pessoa:
     @classmethod
     def nome_e_atributo_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
+class Homem(Pessoa):
+    def cumprimetar(self):
+        cumprimentar_da_classe=super().cumprimetar()
+        return f'{cumprimentar_da_classe}.Aperto de mão!'
+
+
+class Mutante(Pessoa):
+    olhos =  3
 
 if __name__ == '__main__':
-    josevaldo = Pessoa(nome='Josevaldo')
-    joviniano = Pessoa(josevaldo, nome='Joviniano')
+    josevaldo = Mutante(nome='Josevaldo')
+    joviniano = Homem(josevaldo, nome='Joviniano')
     print(Pessoa.cumprimetar(joviniano))
     print(id(joviniano))
     print(joviniano.cumprimetar())
@@ -31,10 +40,16 @@ joviniano.olhos =   1
 del joviniano.olhos
 print(joviniano.__dict__)
 print(josevaldo.__dict__)
-Pessoa.olhos = 3
 print(Pessoa.olhos)
 print(joviniano.olhos)
 print(josevaldo.olhos)
 print(id(Pessoa.olhos)),print(id(joviniano.olhos)), print(id(josevaldo.olhos))
 print(Pessoa.metodo_statico(), joviniano.metodo_statico())
 print(Pessoa.nome_e_atributo_de_classe(), joviniano.nome_e_atributo_de_classe())
+pessoa = Pessoa('Anonimo')
+print(isinstance(pessoa, Homem))
+print(isinstance(josevaldo, Homem))
+print(isinstance(josevaldo, Pessoa))
+print(josevaldo.olhos)
+print(joviniano.cumprimetar())
+print(josevaldo.cumprimetar())
