a2316cdf800aedaeb98c94490ded073d5ed13d0e
willian-virgilio <willian.virgilio@gmail.com>
2022-01-18 18:58:35 -0300

    Aula atributo de dados

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,19 @@
 class Pessoa:
+    def __init__(self,nome=None,idade=35):
+        self.nome =nome
+        self.idade = idade
+        
     def cumprimetar(self):
-        return f'Olá {id(self)}'
+        return f'Ola {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimetar(p))
     print(id(p))
     print(p.cumprimetar())
+    print(p.nome)
+    p.nome = 'Renzo'
+    print(p.nome)
 
 
