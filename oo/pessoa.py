55175f8be56d290b9ca3551900c1e218ff47a342
Fabio Silva <fabiocarini2003@gmail.com>
2022-02-01 18:47:23 -0300

    Criados Aributo complexo filhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=38):
+    def __init__(self, *filhos, nome=None, idade=38):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Luis')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Fabio'
-    print(p.nome)
-    print(p.idade)
+    fabio = Pessoa(nome='Fabio')
+    luis = Pessoa(fabio, nome='Luis')
+    print(Pessoa.cumprimentar(luis))
+    print(id(luis))
+    print(luis.cumprimentar())
+    print(luis.nome)
+    print(luis.idade)
+    for filho in luis.filhos:
+        print(filho.nome)
