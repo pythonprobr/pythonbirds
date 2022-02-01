3fc7694d6a6576cb30101b3b2a40da3fc3e53ccf
Fabio Silva <fabiocarini2003@gmail.com>
2022-02-01 18:36:23 -0300

    Criados Aributos de Instancia nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=38):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luis')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Fabio'
+    print(p.nome)
+    print(p.idade)
