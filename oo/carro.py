1ac6868f5e6ae27487f48ffabc57b778164d28ff
willian-virgilio <willian.virgilio@gmail.com>
2022-02-19 20:55:20 -0300

    car class implemented to compose mode

---
+++ oo/carro.py
@@ -0,0 +1,16 @@
+"""
+Criar um carro pela composição a partir de duas classes
+1)Motor
+2)Direção
+
+O motor terá que controlar a velocidade.
+Ele oferece os seguintes atributos:
+1) Atributo de dados Velocidade
+2) Metodo Acelerar, que deverar incrementar 1 velocidade em 1 unidade
+3) Método de frear que devera decrementar 2 velocidades
+
+A direção deve controlar a direção
+1)Valor de direção é Norte, sul, leste e oeste
+2)Metodo girar_a_direita e girar_a_esquerda
+
+"""
