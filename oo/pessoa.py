1da2185e1546e1a9505b5593d9170def3f970fde
André Rego Rodrigues <andreregor@gmail.com>
2022-02-27 20:43:46 -0300

    Finalizada a Classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,15 @@ class Pessoa:
     def cumprimentar(self):
         return 'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+    
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+
 if __name__ == '__main__':
     renzo = Pessoa(nome='Renzo')
     luciano = Pessoa(renzo, nome='Luciano')    
@@ -30,3 +39,5 @@ if __name__ == '__main__':
     print(luciano.olhos)
     print(renzo.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
