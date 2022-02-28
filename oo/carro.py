822130adb37a936ff8d380b19ed3669cc933ac96
André Rego Rodrigues <andreregor@gmail.com>
2022-02-27 21:21:04 -0300

    Implementada Classe Carro

--- oo/carro.py
+++ oo/carro.py
@@ -74,10 +74,10 @@ O   L
     0
     >>> carro.acelerar()
     >>> carro.calcular_velocidade()
-    2
+    1
     >>> carro.acelerar()
     >>> carro.calcular_velocidade()
-    1
+    2
     >>> carro.frear()
     >>> carro.calcular_velocidade()
     0
@@ -105,8 +105,50 @@ class Motor():
         self.velocidade -= 2
         self.velocidade = max(0, self.velocidade)   
 
-    
-class Direcao():
-    pass
 
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
+
+class Direcao():
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE
+    }
+
+    def __init__(self):
+        self.valor = NORTE
+    
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+    
+    def calcular_direcao(self):
+        return self.direcao.valor
+    
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+    
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
 
