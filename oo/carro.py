9c4c8824391a15f65197a35e52ab3c1db38ac826
willian-virgilio <willian.virgilio@gmail.com>
2022-03-12 13:54:54 -0300

    add dictionary to drive, add dictionary to right and left directions

--- oo/carro.py
+++ oo/carro.py
@@ -116,9 +116,14 @@ class Direcao:
     rotacao_a_direita_dct =  {
         NORTE:LESTE, LESTE:SUL, SUL:OESTE, OESTE:NORTE
     }
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
+    }
     def __init__(self):
         self.valor = NORTE
     def girar_a_direita(self):
         self.valor = self.rotacao_a_direita_dct[self.valor]
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
 
 
