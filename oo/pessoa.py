8fec95def88b5b42e15abea82ff2a9ba5ac2aab3
Vinícius Esmeraldo <vinicius.esmeraldo@gmail.com>
2022-07-25 10:40:02 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=32):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá!{id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Esmeraldo')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Vinícius'
+    print(p.nome)
+    print(p.idade)
