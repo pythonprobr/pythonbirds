79756cc79d7577e67a862aa29f486f2f9f29c6b0
vsantsal <vsantsal@gmail.com>
2022-11-23 22:44:48 -0300

    atributo nome passado no construtor da classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,13 +6,16 @@ __version__ = "0.1.0"
 
 
 class Pessoa:
+    def __init__(self, nome: str) -> None:
+        self.nome = nome
+
     def cumprimentar(self) -> str:
         """
         Retorna uma str com cumprimento a quem invocou a mensagem.
         """
-        return f'Olá {id(self)}'
+        return f'Olá, sou {self.nome}'
 
 
 if __name__ == '__main__':
-    pessoa = Pessoa()
+    pessoa = Pessoa('Renzo')
     print(pessoa.cumprimentar())
